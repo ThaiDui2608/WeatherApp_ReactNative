d029493c3016274789a40f635252d58127bbd8b7
ThaiDui2608 <duisensei2608@gmail.com>
2023-09-07 00:23:39 +0700

    fix bug tailwinds

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 
 const HomeScreen = () => {
   return (
     <View className="flex-1 relative">
-        <StatusBar style='light'/>
-        <Image blurRadius={70} source={require('../assets/images/bg.png')} className='absolute h-full w-full'/>
+      <StatusBar style='light'/>
+      <Image source={require('../assets/images/bg.png')} className='absolute h-full w-full'/>
     </View>
   )
 }
